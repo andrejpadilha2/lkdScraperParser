037cc2c6262a3fdc192bd7c1376c05894916faaa
andrejpadilha2 <andrejpadilha@gmail.com>
2023-06-25 20:48:58 -0300

    Test 'setUp' is now executed only once for all tests. Changed the name of some methods and variables to better express their meaning

--- linkedinProfiles/parser/person.py
+++ linkedinProfiles/parser/person.py
@@ -1,42 +1,42 @@
 from bs4 import BeautifulSoup
 
-def parse_linkedin_name(identification_list):
-    name_item = identification_list.find('h1', class_='top-card-layout__title')
+def parse_linkedin_name(identification_card):
+    name_item = identification_card.find('h1', class_='top-card-layout__title')
     return name_item.text.strip() if name_item else ""
 
-def parse_headline(identification_list):
-    headline_item = identification_list.find('h2', class_='top-card-layout__headline')
+def parse_headline(identification_card):
+    headline_item = identification_card.find('h2', class_='top-card-layout__headline')
     return headline_item.text.strip() if headline_item else ""
 
-def parse_location(identification_list):
-    location_item = identification_list.find('div', class_='top-card__subline-item')
+def parse_location(identification_card):
+    location_item = identification_card.find('div', class_='top-card__subline-item')
     return location_item.text.strip() if location_item else ""
 
-def parse_followers(identification_list):
-    followers_item = identification_list.find('span', class_='top-card__subline-item')
+def parse_followers(identification_card):
+    followers_item = identification_card.find('span', class_='top-card__subline-item')
     return followers_item.text.strip() if followers_item else ""
 
-def parse_connections(identification_list):
-    connections_item = identification_list.find('span', class_='top-card__subline-item--bullet')
+def parse_connections(identification_card):
+    connections_item = identification_card.find('span', class_='top-card__subline-item--bullet')
     return connections_item.text.strip() if connections_item else ""
 
 def parse_about(soup):
     about_item = soup.find('section', class_='summary')
     return about_item.find('div', class_='core-section-container__content').text.strip() if about_item else ""
 
-def get_identification_list(soup):
+def get_identification_card(soup):
     return soup.find('section', class_='top-card-layout')
 
 def extract_person_info(page_source):
     soup = BeautifulSoup(page_source, 'html.parser')
 
-    identification_list = get_identification_list(soup)
+    identification_card = get_identification_card(soup)
 
-    name = parse_linkedin_name(identification_list)
-    headline = parse_headline(identification_list)
-    location = parse_location(identification_list)
-    followers = parse_followers(identification_list)
-    connections = parse_connections(identification_list)
+    name = parse_linkedin_name(identification_card)
+    headline = parse_headline(identification_card)
+    location = parse_location(identification_card)
+    followers = parse_followers(identification_card)
+    connections = parse_connections(identification_card)
     about = parse_about(soup)
     
     person_info = {
--- linkedinProfiles/tests/parser/test_person.py
+++ linkedinProfiles/tests/parser/test_person.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import json
 from bs4 import BeautifulSoup
 
-from ...parser.person import extract_person_info, get_identification_list, parse_about, parse_connections, parse_followers, parse_headline, parse_location, parse_linkedin_name
+from ...parser.person import extract_person_info, get_identification_card, parse_about, parse_connections, parse_followers, parse_headline, parse_location, parse_linkedin_name
 
 class CustomTestResult(unittest.TextTestResult):
     def addSuccess(self, test):
@@ -16,66 +16,67 @@ class CustomTestRunner(unittest.TextTestRunner):
     resultclass = CustomTestResult
 
 class TestExtractPersonInfo(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.maxDiff = None
         self.test_data_path = Path(__file__).resolve().parent / 'test_data'
-        test_data_file = self.test_data_path / 'expected_person_data.json'
+        expected_data_file = self.test_data_path / 'expected_data' / 'expected_person_data.json'
 
-        with open(test_data_file, 'r') as f:
-            test_data = json.load(f)
-            self.profiles = test_data['profiles']
+        with open(expected_data_file, 'r') as f:
+            expected_data = json.load(f)
+            self.profiles = expected_data['profiles']
 
-        # load html content and parsed identification_list during setup
+        # load html content and parsed identification_card during setup
         for profile in self.profiles:
-            html_path = self.test_data_path / profile['file']
+            html_path = self.test_data_path / 'input_data' / profile['file']
             with open(html_path, "r") as f:
                 page_source = f.read()
             soup = BeautifulSoup(page_source, 'html.parser')
-            profile['identification_list'] = get_identification_list(soup)
+            profile['identification_card'] = get_identification_card(soup)
             profile['soup'] = soup
             profile['page_source'] = page_source
 
     def num_profiles_tested(self):
         return len(self.profiles)
 
-    def test_parse_name(self):
+    def test_parse_linkedin_name(self):
         """Test if name is parsed correctly."""
         for profile in self.profiles:
-            parsed_name = parse_linkedin_name(profile['identification_list'])
+            parsed_name = parse_linkedin_name(profile['identification_card'])
             expected_name = profile['data']['linkedin_name']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_name, expected_name)
 
     def test_parse_headline(self):
         """Test if headline is parsed correctly."""
         for profile in self.profiles:
-            parsed_headline = parse_headline(profile['identification_list'])
+            parsed_headline = parse_headline(profile['identification_card'])
             expected_headline = profile['data']['headline']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_headline, expected_headline)
 
     def test_parse_location(self):
         """Test if location is parsed correctly."""
         for profile in self.profiles:
-            parsed_location = parse_location(profile['identification_list'])
+            parsed_location = parse_location(profile['identification_card'])
             expected_location = profile['data']['location']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_location, expected_location)
 
     def test_parse_followers(self):
         """Test if followers are parsed correctly."""
         for profile in self.profiles:
-            parsed_followers = parse_followers(profile['identification_list'])
+            parsed_followers = parse_followers(profile['identification_card'])
             expected_followers = profile['data']['followers']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_followers, expected_followers)
 
     def test_parse_connections(self):
         """Test if connections are parsed correctly."""
         for profile in self.profiles:
-            parsed_connections = parse_connections(profile['identification_list'])
+            parsed_connections = parse_connections(profile['identification_card'])
             expected_connections = profile['data']['connections']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_connections, expected_connections)
 
     def test_parse_about(self):
@@ -83,15 +84,16 @@ class TestExtractPersonInfo(unittest.TestCase):
         for profile in self.profiles:
             parsed_about = parse_about(profile['soup'])
             expected_about = profile['data']['about']
-            with self.subTest(i=profile['file']):
+            with self.subTest(profile=profile['file']):
                 self.assertEqual(parsed_about, expected_about)
     
     def test_extract_person_info(self):
+        """Test if all person info is parsed and structured in a dictionary."""
         for profile in self.profiles:
             extracted_person_info = extract_person_info(profile['page_source'])
-            expected__person_info = profile['data']
-            with self.subTest(i=profile['file']):
-                self.assertEqual(extracted_person_info, expected__person_info)
+            expected_person_info = profile['data']
+            with self.subTest(profile=profile['file']):
+                self.assertEqual(extracted_person_info, expected_person_info)
 
 if __name__ == '__main__':
     unittest.main(testRunner=CustomTestRunner())
