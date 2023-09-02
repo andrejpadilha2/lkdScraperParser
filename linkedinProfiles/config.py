2363ac5e40b1f5e1de159b51b3b18cd93ebbee32
andrejpadilha2 <andrejpadilha@gmail.com>
2023-09-02 11:17:45 -0300

    More config options to: 1) manually solve captchas and 2) continue from last run

--- linkedinProfiles/config.py
+++ linkedinProfiles/config.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
-PROFILES_FOLDER_NAME = 'profilesSelenium19_1000'  # << change only this!!! profilesSelenium18_og_namesake
+PROFILES_FOLDER_NAME = 'profilesSelenium24_7100'  # << change only this!!! profilesSelenium18_og_namesake
 # PROFILES_FOLDER_NAME = 'test'
 HEADLESS = True
 
-# True if the scraper should retry profiles that failed in a previous run 
-# because a Captcha was raised. 
-RETRY_CAPTCHA_WHEN_RESUMING = False 
+# True if the scraper should continue from where it stopped last time,
+# False if it should start from the beginning (it will retry failed name_variations 
+# and skip already scrapped ones) 
+CONTINUE_FROM_LAST_RUN = True
 
 
-
+MANUAL_CAPTCHA = False # True: you will be prompted to solve captchas manually (browser will be started in HEADED mode, regardless of the HEADLESS option set above). False: you won't.
 
 
 BASE_PATH = Path(__file__).resolve().parent
